570538f412bd9bb6179f0a9bef1148c67bd9e7f3
samFabrice <dsamfabrice5@gmail.com>
2021-07-02 12:17:58 +0200

    I made changes to the class constructor in order to initialize servo motosrs when a switch object is created I delete in the switch.msg the message biais_info

--- src/switch/src/switch_pilot.py
+++ src/switch/src/switch_pilot.py
@@ -1,10 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+    Copyright (C) 2021  CNRS
+    This file is part of "Speedlib".
+    "Speedlib" is an API built for the use case of autonomous navigation.
+    It has  been developed to control quay cranes and trains of multimodal
+    waterborne Lab as part of The SPEED project, a project which aims to
+    enhance and support the growth of a system of connected port solutions,
+    with the use of data science and IoT (Internet of Things) technologies.
+    The library allows controlling the motion of the IoT devices at H0 scale
+    in automatic mode, in three directions and exchanging with the information
+    system for overall management
+"""
+
+
+#======= Import ================
+import sys
+import signal
 import rospy
 from switch.msg import switch as switch_message
 from speedlib.dcc import dcc_object, dcc_switches
 from speedlib.dcc.dcc_object import DCCObject
-from speedlib.dcc .dcc_switches import Switch
-import sys
-import signal
+from speedlib.dcc.dcc_switches import Switch
+
 
 def start_controller():
     dcc_object.start()
@@ -15,11 +33,10 @@ def stop_controller(signal, frame):
 
 class SwitchPiloteNode:
     
-    def __init__(self, num_switch, biais_id):
-        num_switch = 3
+    def __init__(self, num_switch):
         self.switch ={}
-        for i in range(1, num_switch):
-            self.switch[i] = Switch("DCC"+str(i), i, biais_id)
+        for i in range(1, num_switch+1):
+            self.switch[i] = Switch("DCC"+str(i), i)
     
     def callback(self, data):
         print(data)
@@ -27,25 +44,26 @@ class SwitchPiloteNode:
         if not isinstance(data.switch_name, str):
             raise TypeError("Switch_name must be a str but got "+str(data.switch_name))
         
-        if not isinstance(data.switch_number, int):
-            raise TypeError("Switch_number must be an int but got "+str(data.switch_number))
+        if not isinstance(data.switch_address, int):
+            raise TypeError("Switch_address must be an int but got "+str(data.switch_address))
 
         if not isinstance(data.biais_id, int):
-            raise TypeError("biais_id must be a str but got "+str(data.biais_id))
+            raise TypeError("biais_id  be a int but got "+str(data.biais_id))
         
         if data.biais_id not in [1, 2]:
             raise ValueError("biais_id must be 1 or 2 but got :" +str(data.biais_id))
         
         if data.switch_command == "biais":
-            self.switch[data.switch_number].biais = data.biais_state
+            self.switch[data.switch_address].biais = [data.biais_id ,data.biais_state]
         
         if data.switch_command == "biais_info":
-            print(self.switch[data.switch_number])
+            print(self.switch[data.switch_address])
 
 if __name__=='__main__':
 
    start_controller()
-   switchnode = SwitchPiloteNode()
+   print(type(sys.argv[1]))
+   switchnode = SwitchPiloteNode(int(sys.argv[1]))
    rospy.init_node('switch', anonymous=True)
    print("Initialisation du noeud")
    rospy.Subscriber("switch/command", switch_message, switchnode.callback)
