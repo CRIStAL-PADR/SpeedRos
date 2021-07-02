fe179def9eeba563363496fd2c4987cf88432eb1
samFabrice <dsamfabrice5@gmail.com>
2021-07-02 11:46:11 +0200

    Correction apportée aux differents fichiers afin d'exécuter le noeud ros train_pilot.py

--- src/train/src/train_pilot.py
+++ src/train/src/train_pilot.py
@@ -3,84 +3,86 @@
 
 #======= Import ================
 import rospy
-from train.msg import train
+from train.msg import train as train_message
 from speedlib.trains import dcc
 from speedlib.trains.dcc import Train
 
 
+def start_locomotive():
+	dcc.start()
+
+def stop_locomotive():
+	dcc.stop()
+
     
 class TrainPiloteNode:
 
-    def __init__(self):
-        self.object = None
+	def __init__(self):
+		self.object = None
+		self.data = None
+		self.train = {}
     
 
-    def train_init(self):
-        train = {}
-        MAX_NUMBER = 20
-        for i in range(1, MAX_NUMBER):
-            train[i] = Train("DCC"+str(i), i)
-        return train
+	def train_init(self):
+		MAX_NUMBER = 3
+		for i in range(1, MAX_NUMBER):
+			self.train[i] = Train("DCC"+str(i), i)
+		
 
-    def callback(self, data):
+	def callback(self, data):
 
-        print(self.data)
-        if not isinstance(self.data.train_name, str):
-            raise TypeError("train_name must be a str but got" +str(self.data.train_name))
+		print(data)
+		self.object = data
+		self.data = data
+		if not isinstance(data.train_name, str):
+			raise TypeError("train_name must be a str but got" +str(data.train_name))
 
-        if not isinstance(self.data.train_number, int):
-            raise TypeError(" train adress must be a str but got" +str(self.data.train_number))
+		if not isinstance(data.train_number, int):
+			raise TypeError(" train adress must be a str but got" +str(data.train_number))
+		if self.object.train_command == "faster":
+            		self.train[self.object.train_number].faster()
 
-        self.object = data  
+		if self.data.train_command == "slower":
+			self.train[self.object.train_number].slower()
+
+		if self.data.train_command == "speed" and self.data.speed_value !=0:
+			self.train[self.object.train_number].speed = self.object.speed_value
+        
+		if self.data.train_command == "reverse":
+			self.train[self.object.train_number].reverse()
+        
+		if self.data.train_command == "f1":
+			self.train[self.object.train_number].f1 = self.object.accessories_value
+        
+		if self.data.train_command == "f2":
+			self.train[self.object.train_number].f2 = self.object.accessories_value
+
+		if self.data.train_command == "f3":
+			self.train[self.object.train_number].f3 = self.object.accessories_value
     
-    def start_locomotive(self):
-        dcc.start
+		if self.data.train_command == "f4":
+			self.train[self.object.train_number].f4 = self.object.accessories_value
 
-    def stop_locomotive(self):
-        dcc.stop()
+		if self.data.train_command == "fl":
+			self.train[self.object.train_number].fl = self.object.accessories_value 
 
-    def listener(self):
-        train= {}
-        train= self.train_init() 
+		if self.data.train_command == "train_info":
+			print(self.train[self.object.train_number])
 
-        rospy.init_node('listener', anonymous=True)
 
-        rospy.Subscriber("train/command", train, self.callback)
-        
-        if self.object.train_command == "faster":
-            train[self.object.train_number].faster()
+	def listener(self):              
+	                                                                #je crée une variablee train qui va pointer vers une zone mémoire ou j'initialise un 													doctionnaire vide
+		self.train_init() 							  #J'initialise tous les trains et je fais pointer la variable train crée précédement 													vers la  zone mémoire pointer par la fonction
 
-        if self.data.train_command == "slower":
-            train[self.object.train_number].slower()
+		rospy.init_node('listener', anonymous=True)				
 
-        if self.data.train_command == "speed" and self.data.speed_value !=0:
-            train[self.object.train_number].speed = self.object.speed_value
-        
-        if self.data.train_command == "reverse":
-            train[self.object.train_number].reverse()
-        
-        if self.data.train_command == "f1":
-            train[self.object.train_number].f1 = self.object.accessories_value
-        
-        if self.data.train_command == "f2":
-            train[self.object.train_number].f2 = self.object.accessories_value
+		rospy.Subscriber("train/command", train_message, self.callback) 
 
-        if self.data.train_command == "f3":
-            train[self.object.train_number].f3 = self.object.accessories_value
-    
-        if self.data.train_command == "f4":
-            train[self.object.train_number].f4 = self.object.accessories_value
-
-        if self.data.train_command == "fl":
-            train[self.object.train_number].fl = self.object.accessories_value 
-
-        if self.data.train_command == "train_info":
-            print(train[self.object.train_number])       
-
-        rospy.spin()
+		rospy.spin()
 
 if __name__=='__main__':
 
-    TrainPiloteNode.start_locomotive()
-    TrainPiloteNode.listener()
-    TrainPiloteNode.stop_locomotive
+	start_locomotive() # j'excécute la fonction start_locomotive
+	train = TrainPiloteNode() # j'instancie un objet de type trainPiloteNode et je fais pointer la variable train vers la zone mémoir eou se trouve l'objet instancié
+	train.listener() # J
+	stop_locomotive()
