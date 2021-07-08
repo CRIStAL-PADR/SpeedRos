bdc15d9449dd63e969c26bcc441755bb9bb18bfb
samFabrice <dsamfabrice5@gmail.com>
2021-07-08 11:21:07 +0200

    Updated the callback function of crane_pilote.py

--- src/crane/src/crane_pilot.py
+++ src/crane/src/crane_pilot.py
@@ -7,7 +7,6 @@ import rospy
 from crane.msg import crane as crane_message
 from speedlib.cranes import faller
 from speedlib.cranes.faller import Crane
-from pint import UnitRegistry
 import sys
 
 class CranePiloteNode:
@@ -21,37 +20,47 @@ class CranePiloteNode:
 
     def callback(self,data):
 
-        if data.motors_name in ["MotorChassis", "MotorSpreader", "MotorCrab"]:
-            if data.motors_direction in ["MotorDirectionBackward", "MotorDirectionForward"]:
+        if data.crane_command not in ["start", "stop", "step", "start_for", "battery", "change_speed", "get_speed", "set_speed"]:
+            raise RuntimeError("Erreur dans la commande adressé au moteur : "+str(data.crane_command))
 
-                if data.crane_command == "start":
-                    self.crane.start(self.motors[data.motors_name], faller.data.motor_direction)
+        if data.motors_name not in ["MotorChassis", "MotorSpreader", "MotorCrab"]:
+            raise RuntimeError("motors_name must be MotorChassis, MotorSpreader, MotorCrab but got "+str(data.motors_name))
 
-                elif data.crane_command == "stop":
-                    self.crane.stop(self.motors[data.motors_name])
+        if not isinstance(data.values, int):
+            raise TypeError("data.values must be an integer but got :"+str(data.values))
 
-                elif data.crane_command == "step":
-                    self.crane.step(self.motors[data.motors_name] faller.data.motors_direction)
+        if data.motors_direction not in ["MotorDirectionBackward", "MotorDirectionForward"]:
+            raise RuntimeError("motor direction must be MotorDirectionForward or MotorDirectionBackward but got "+str(data.motors_direction))
 
-                elif data.crane_command == "start_for":
-                    self.crane.start_for(data.values*ureg.second, self.motors[data.motors_name],
-                                           faller.data.motors_direction)
+        if data.crane_command == "start":
+            self.crane.start(self.motors[data.motors_name], self.motors_direction[data.motors_direction])
 
-            elif data.crane_command == "battery":
-                print(self.crane.battery)
+        elif data.crane_command == "stop":
+            self.crane.stop(self.motors[data.motors_name])
 
-            elif data.crane_command == "change_speed":
-                self.crane.change_speed(self.motors[data.motors_name], data.values)
+        elif data.crane_command == "step":
+            self.crane.step(self.motors[data.motors_name], self.motors_direction[data.motors_direction])
 
-            elif data.crane_command == "get_speed":
-                print(self.crane.get_speed(self.motors[data.motors_name]))
+        elif data.crane_command == "start_for":
+            self.crane.start_for(data.values* faller.ureg.second, self.motors[data.motors_name],
+                                            self.motors_direction[data.motors_direction])
 
-            elif data.crane_command == "set_speed":
-                print(self.crane.set_speed(self.motors[data.motors_name], data.values))
+        elif data.crane_command == "battery":
+            print(self.crane.battery)
+
+        elif data.crane_command == "change_speed":
+            print(self.crane.change_speed(self.motors[data.motors_name], data.values))
+
+        elif data.crane_command == "get_speed":
+            print(self.crane.get_speed(self.motors[data.motors_name]))
+
+        elif data.crane_command == "set_speed":
+            print(self.crane.set_speed(self.motors[data.motors_name], data.values))
+
 
 
 if __name__=='__main__':
     cranenode = CranePiloteNode(sys.argv[1])
-    rospy.init_node('crane', anonymous=True)
-    rospy.Subscriber("crane/command", crane_message, cranenode.callback)
+    rospy.init_node(sys.argv[2], anonymous=True)
+    rospy.Subscriber(sys.argv[3], crane_message, cranenode.callback)
     rospy.spin()
