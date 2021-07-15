5e67fb12d8e503335ccd7eb9551ce077b90a669a
samFabrice <dsamfabrice5@gmail.com>
2021-07-15 11:12:06 +0200

    Mis a jour des fichiers train_pilot.py et switch.msg

--- src/train/src/train_pilot.py
+++ src/train/src/train_pilot.py
@@ -4,25 +4,26 @@
 #======= Import ================
 import rospy
 from train.msg import train as train_message
-from speedlib.trains import dcc
-from speedlib.trains.dcc import Train
+from speedlib.dcc import dcc_object, dcc_trains
+from speedlib.dcc.dcc_object import DCCObject
+from speedlib.dcc .dcc_trains import Train
 import sys
 import signal
 
-def start_locomotive():
-	dcc.start()
+def start_controller():
+	dcc_object.start()
 
 def stop_controller(signal,frame):
-	dcc.stop()
+	dcc_object.stop()
 	sys.exit(0)
- 
+
 class TrainPiloteNode:
 
 	def __init__(self, num_train=3):
 		self.train = {}
 		for i in range(1, num_train):
 			self.train[i] = Train("DCC"+str(i), i)
-    		
+
 	def callback(self, data):
 		print(data)
 		if not isinstance(data.train_name, str):
@@ -38,42 +39,38 @@ class TrainPiloteNode:
 
 		if data.train_command == "speed" and self.data.speed_value !=0:
 			self.train[data.train_number].speed = data.speed_value
-        
+
 		if data.train_command == "reverse":
 			self.train[data.train_number].reverse()
-        
+
 		if data.train_command == "f1":
 			self.train[data.train_number].f1 = data.accessories_value
-        
+
 		if data.train_command == "f2":
 			self.train[data.train_number].f2 = data.accessories_value
 
 		if data.train_command == "f3":
 			self.train[data.train_number].f3 = data.accessories_value
-    
+
 		if data.train_command == "f4":
 			self.train[data.train_number].f4 = data.accessories_value
 
 		if data.train_command == "fl":
-			self.train[data.train_number].fl = data.accessories_value 
+			self.train[data.train_number].fl = data.accessories_value
 
 		if data.train_command == "train_info":
 			print(self.train[data.train_number])
-		
 
-	
+
+
 if __name__=='__main__':
 
-	dcc.start() # j'excécute la fonction start_locomotive
-	trainnode = TrainPiloteNode()						
-	rospy.init_node('toto', anonymous=True)
+	start_controller() # j'excécute la fonction start_locomotive
+	trainnode = TrainPiloteNode()
+	rospy.init_node('train', anonymous=True)
 	print("Initialisation du noeud")
 	rospy.Subscriber("train/command", train_message, trainnode.callback)
-	
+
 	signal.signal(signal.SIGINT,stop_controller)
 
 	rospy.spin()
-	
-
-
-
