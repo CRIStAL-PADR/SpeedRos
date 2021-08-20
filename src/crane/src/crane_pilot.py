4ae1df6e5c856ee51fb4740b913136f00bd6e724
samFabrice <dsamfabrice5@gmail.com>
2021-08-20 12:39:22 +0200

    Modification of the crane_pilot.py file in order to use string type messages to control the cranes

--- src/crane/src/crane_pilot.py
+++ src/crane/src/crane_pilot.py
@@ -20,27 +20,27 @@ from speedlib.cranes import faller
 from speedlib.cranes.faller import Crane
 
 class CranePiloteNode:
-    """ 
+	""" 
         This class is used to create a ROS node capable of controlling cranes
         with ROS
 	"""
 
-    def __init__(self, i_p="172.17.217.217"):
-        """
+	def __init__(self, i_p="172.17.217.217"):
+		"""
         Parameters
         ----------
         i_p : TYPE, optional
             DESCRIPTION. The default is "172.17.217.217".
             It corresponds to the ip address of the crane
-        """
-        self.i_p = i_p
-        self.crane = Crane()
-        self.crane.init(self.i_p)
-        self.motors = {"MotorCrab": 1, "MotorSpreader": 3, "MotorChassis" : 2}
-        self.motors_direction = {"MotorDirectionForward": 1, "MotorDirectionBackward":-1}
+		"""
+		self.i_p = i_p
+		self.crane = Crane()
+		self.crane.init(self.i_p)
+		self.motors = {"MotorCrab": 1, "MotorSpreader": 3, "MotorChassis" : 2}
+		self.motors_direction = {"MotorDirectionForward": 1, "MotorDirectionBackward":-1}
 
-    def process_data(self, data):
-        """
+	def process_data(self, data):
+		"""
         Get the command in the form of a string sent by the user and transform it
         in dictionary which will be used in by the callback method in order to execute the
         corresponding command
@@ -52,82 +52,57 @@ class CranePiloteNode:
 	    -------
 	    data_dict : dict
 	        Command return after processing
-        """
-        data_split = data.split(";")
-        print("data_split : ", data_split)
-        data_dict = {}
-        for i in range(len(data_split)):
-            buffer = data_split[i].split(":")
-            data_dict[buffer[0].strip()] = buffer[1].strip()
-        return data_dict
+		"""
+		data_split = data.split(";")
+		print("data_split : ", data_split)
+		data_dict = {}
+		for i in range(len(data_split)):
+			buffer = data_split[i].split(":")
+			data_dict[buffer[0].strip()] = buffer[1].strip()
+		return data_dict
 
-    def callback(self,data):
-        """
+	def callback(self,data):
+		"""
         Parameters
         ----------
         data :  crane.msg
             DESCRIPTION : It is the argument of the callback function containing 
             the messages
 
-        Returns
-        -------
-        None.
+		"""
+		command = self.process_data(data.data)
+		print(command["crane_command"])
+		
+		if command["crane_command"] == "start":
+			self.crane.start(self.motors[command["motors_name"]], 
+								self.motors_direction[command["motors_direction"]])
 
-        """
-        command = self.process_data(data.data)
-
-        if command["crane_command"] not in ["start", "stop", "step", "start_for",
-                                      "battery", "change_speed", "get_speed", "set_speed"]:
-            raise RuntimeError("Erreur dans la commande adressé au moteur : "+
-                               str(command["crane_command"])
-
-        if command["motors_name"] not in ["MotorChassis", "MotorSpreader", "MotorCrab"]:
-            raise RuntimeError("""motors_name must be MotorChassis, MotorSpreader,
-                               MotorCrab but got """+str(command["motors_name"])
-
-        if not isinstance(int(command["values"]), int):
-            raise TypeError("data.values must be an integer but got :"+str(command["values"]))
-
-        if command["motors_direction"] not in ["MotorDirectionBackward", "MotorDirectionForward"]:
-            raise RuntimeError("""motor direction must be MotorDirectionForward or
-                               MotorDirectionBackward but got """+str(command["motors_direction"]))
-
-        """if data.crane_command == "start":
-            self.crane.start(self.motors[data.motors_name],
-                             self.motors_direction[data.motors_direction])
-
-        elif data.crane_command == "stop":
-            self.crane.stop(self.motors[data.motors_name])
-
-        elif data.crane_command == "step":
-            self.crane.step(self.motors[data.motors_name],
-                            self.motors_direction[data.motors_direction])
-
-        elif data.crane_command == "start_for":
-            self.crane.start_for(data.values* faller.ureg.second, self.motors[data.motors_name],
-                                            self.motors_direction[data.motors_direction])
-
-        elif data.crane_command == "battery":
-            print(self.crane.battery)
-
-        elif data.crane_command == "change_speed":
-            print(self.crane.change_speed(self.motors[data.motors_name], data.values))
-
-        elif data.crane_command == "get_speed":
-            print(self.crane.get_speed(self.motors[data.motors_name]))
-
-        elif data.crane_command == "set_speed":
-            print(self.crane.set_speed(self.motors[data.motors_name], data.values))"""
-
-
-
+		elif command["crane_command"] == "stop":
+			self.crane.stop(self.motors[command["motors_name"]])
+			
+		elif command["crane_command"] == "step":
+			self.crane.start(self.motors[command["motors_name"]], 
+								self.motors_direction[command["motors_direction"]])
+		
+		elif command["crane_command"] == "start_for":
+			self.crane.start_for(float(command["value"]) * faller.ureg.second,
+						self.motors[command["motors_name"]], 
+								self.motors_direction[command["motors_direction"]])
+		elif command["crane_command"] == "battery":
+			print(self.crane.battery)
+			
+		elif command["crane_command"] == "change_speed":
+			print(self.crane.change_speed(self.motors[command["motors_name"]], int(command["speed_value"])))
+		
+		elif command["crane_command"] == "get_speed":
+			print(self.crane.get_speed(self.motors[command["motors_name"]]))
+		
+		elif command["crane_command"] == "set_speed":
+			print(self.crane.set_speed(self.motors[command["motors_name"]], int(command["speed_value"])))
+	
 if __name__=='__main__':
-    if len(sys.argv) <4 :
-        raise RuntimeError(""" To run crane_pilote needs 3 input arguments:
-                               ip address, the message and the command""")
-
-    cranenode = CranePiloteNode(sys.argv[1])
-    print("Initialisation du noeud")
-    rospy.init_node(sys.argv[2], anonymous=True)
-    rospy.Subscriber(sys.argv[3], String, cranenode.callback)
-    rospy.spin()
+	cranenode = CranePiloteNode(sys.argv[1])
+	print("Initialisation du noeud")
+	rospy.init_node('crane', anonymous=True)
+	rospy.Subscriber('crane/command', String, cranenode.callback)
+	rospy.spin()
